f40281cc8711094faa1fca618bb90b4fc2b327f1
Anwar77-b <84320105+Anwar77-b@users.noreply.github.com>
2025-04-08 14:49:29 +0100

    ee

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,11 +1,15 @@
 import connectDB from "@/lib/mongodb";
 import { NextResponse } from "next/server";
+import Product from "@/lib/models/Product";
 
-export async function GET() {
+export async function POST(req) {
+  // return NextResponse.json({ jj: "jj" });
   try {
+    const res = await req.json();
+    const products = await Product.find({ _id: { $in: res } });
     await connectDB();
 
-    return NextResponse.json();
+    return NextResponse.json({ products });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
